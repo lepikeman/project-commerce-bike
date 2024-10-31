2473de2925ec70ce6ad838afb38616751b5ac0ee
lepikeman <axelx2010@gmail.com>
2024-10-31 15:41:24 +0100

    change stripe module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,7 @@ import { UsersModule } from './user/users.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
 import { pgConfig } from './pgConfig';
-import { StripeService } from './stripe/stripe.service';
-import { StripeController } from './stripe/stripe.controller';
+import { StripeModule } from './stripe/stripe.module';
 
 dotenv.config();
 
@@ -19,9 +18,8 @@ dotenv.config();
     UsersModule,
     AuthModule,
     OrderModule,
+    StripeModule,
   ],
-  providers: [StripeService],
-  controllers: [StripeController],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
