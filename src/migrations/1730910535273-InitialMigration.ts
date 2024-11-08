e2825343c3e614ecbebbd29e5a8fc6ae6ed76575
lepikeman <63370994+lepikeman@users.noreply.github.com>
2024-11-08 17:26:16 +0100

    change entity

--- src/migrations/1730910535273-InitialMigration.ts
+++ src/migrations/1730910535273-InitialMigration.ts
@@ -8,21 +8,25 @@ export class InitialMigration1730910535273 implements MigrationInterface {
           username VARCHAR(100) NOT NULL,
           password VARCHAR(100) NOT NULL,
           email_user VARCHAR(100) NOT NULL,
-          hashedRefreshTokens VARCHAR(100),
+          hashedRefreshToken VARCHAR(100),
           deletedAt DATE
       );
       CREATE TABLE tb_products (
           id SERIAL PRIMARY KEY,
-          name VARCHAR(100) NOT NULL,
-          price NUMERIC NOT NULL
+          product_name VARCHAR(100) NOT NULL,
+          price NUMERIC NOT NULL,
+          description VARCHAR(255) NOT NULL,
+          factorynew BOOLEAN NOT NULL,
       );
       CREATE TABLE tb_orders (
           id SERIAL PRIMARY KEY,
-          userId INT REFERENCES tb_users(id),
-          productId INT REFERENCES tb_products(id),
+          user_id INT REFERENCES tb_users(id),
+          product_id INT REFERENCES tb_products(id),
           delivered BOOLEAN NOT NULL,
           deletedAt DATE
-      );
+          order_date DATE
+          paid BOOLEAN NOT NULL,
+      );git 
     `);
   }
 
